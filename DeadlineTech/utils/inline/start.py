0b4b23b112e4b18e6d89403b3d3d069aed2d0267
ordmanthan <ordmanthan@gmail.com>
2025-10-30 12:24:37 +0530

    Clean up button layout by removing unused button
    Removed unused InlineKeyboardButton from the button layout.

--- DeadlineTech/utils/inline/start.py
+++ DeadlineTech/utils/inline/start.py
@@ -11,12 +11,7 @@ def start_panel(_):
             ), 
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=
-            )
-        ]
+    
     ]
     return buttons
 
@@ -41,7 +36,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL), 
-            InlineKeyboardButton(text=_["S_B_5"], url=)
         ]
     ]
     return buttons
